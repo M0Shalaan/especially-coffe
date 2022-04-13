ad28b76ab924edf86c6e453e9d6a142317c91689
Mohamed Shalaan <58323465+M0Shalaan@users.noreply.github.com>
2022-04-14 01:01:09 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,21 @@
+function function1() {
+  alert("We will deliver as soon as possible");
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+function modes() {
+  var element = document.getElementById("dark2");
+  element.classList.toggle("dark-mode");
+}
